10160ed392a9d166f6fb94d43e98706aa813edfe
Ethan Escalante <ethan65g@gmail.com>
2024-09-25 20:30:38 -0700

    added adj list and edges

--- adjList.cpp
+++ adjList.cpp
@@ -4,27 +4,46 @@
 
 #include "adjList.hpp"
 
+
+bool adjList::areWordsAdjacent(const std::string &word1, const std::string &word2) {
+    int differenceCount = 0;
+    for (int i = 0; i < word1.length(); ++i) {
+        if (word1[i] != word2[i]) {
+            ++differenceCount;
+            if (differenceCount > 1) {
+                return false;
+            }
+        }
+    }
+    return differenceCount == 1;
+}
+
+std::string adjList::getVertexWord(int index) const {
+    return vertices[index].word;  // Return the word at the given vertex index
+}
+
+
 int adjList::addVertex(const Vertex& vertex) {
     vertices.push_back(vertex);
     adjList.push_back(std::vector<int>());
 }
 
 void adjList::addEdge(int v1, int v2) {
-    //you have to add both vertices to both adjLists
-    if( v1 < adjList.size() && v2 < adjList.size()){
-        adjList[v1].push_back(v2);
-    }else{
-        std::cout << "OOB indicies" << std::endl;
-    }
+    adjList[v1].push_back(v2);
+    adjList[v2].push_back(v1);
 }
 
 void adjList::printGraph() const {
-    for(int i = 0; i < vertices.size(); i++){
-        std::cout << i << " (" << vertices[i].word << ")";
+    for(int i = 0; i < adjList.size(); i++){
+        std::cout << i << ": ";
         for(int neighbor : adjList[i]){
-            std::cout << neighbor << " (" << vertices[neighbor].word << ") ";
+            std::cout << neighbor << " ";
         }
         std::cout << std::endl;
     }
+}
+
+int adjList::size() const{
+    return adjList.size();
 }
 
